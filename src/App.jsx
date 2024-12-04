1f360c3fee4ef39216de2c56b9471771a177a6d0
Prytula Olexandr <liga5872@gmail.com>
2024-12-04 19:04:27 +0200

    add end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from './routes/AppRoutes.jsx';
 
 function App() {
     return (
